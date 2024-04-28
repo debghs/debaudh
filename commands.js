3f03c855f37ed0067e0715b5883f65f87372599b
debghs <145260557+debghs@users.noreply.github.com>
2024-04-28 22:38:08 +0530

    Add files via upload

---
+++ commands.js
@@ -0,0 +1,39 @@
+// commands.js
+
+const commands = {
+  help: `
+      <p></p>
+      <p>about</p>
+      <p>projects</p>
+      <p>contact</p>
+      <p>clear</p>
+      <p>help</p>
+    `,
+  about: `
+      <div class="about-section">
+        <img src="assets/me.jpeg" alt="debaudh(me)" class="about-image">
+        <div class="about-text">
+          <text>about me:</text><br>
+          <text>sophomore at JU.</text><br>
+          <text>passionate about everything logical.</text><br>
+        </div>
+      </div>
+    `,
+  projects: `
+      <text>projects:</text><br>
+      <ul>
+        <li>project 1</li>
+        <li>project 2</li>
+        <li>project 3</li>
+      </ul>
+    `,
+  contact: `
+      <p>contact me via:</p>
+      <p><a href="mailto:dghosh31428@gmail.com"> ~mail </a></p>
+      <p><a href="tel:+919903147702"> ~phone </a></p>
+      <p><a href="https://in.linkedin.com/in/debaudh-ghosh-38b0472b1" target="_blank"> ~linkedin </a></p>
+    `,
+  clear: ''
+};
+
+export default commands;
